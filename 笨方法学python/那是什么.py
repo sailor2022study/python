86fdd7bed44c7178af92602832f7ddaa4a9e55e5
Raychen <cbt2301141576@gmail.com>
2022-11-11 22:34:00 +0800

    go

---
+++ 笨方法学python/那是什么.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+"I am 6'2\" tall."#将字符串中的双引号转义
+'I am 6\'2" tall.'#将字符串中的单引号转义
